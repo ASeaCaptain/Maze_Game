722ec19ae1373523bf92cc8b2e3435a5c03b0f5a
rhit-jfang <jerryfang954@gmail.com>
2026-05-05 21:11:07 -0400

    updated player and enemy to implement inheritance

--- CSSE220-Final-Project-Starter-main/src/model/Enemy.java
+++ CSSE220-Final-Project-Starter-main/src/model/Enemy.java
@@ -1,41 +1,9 @@
 package model;
 
-import java.awt.Graphics2D;
-
-public class Enemy extends Entity{
-	
-	private int row;
-	private int col;
-	private static final int TILE_SIZE = 40;
+public class Enemy extends Entity {
 	
 	public Enemy(int row, int col) {
 		super(row, col);
 	}
-
-//	/**
-//	 * @return the row
-//	 */
-//	public int getRow() {
-//		return row;
-//	}
-//
-//	/**
-//	 * @return the col
-//	 */
-//	public int getCol() {
-//		return col;
-//	}
-//	
-//	public void drawOn(Graphics2D g2) {
-//		int x = this.col * TILE_SIZE;
-//		int y = this.row * TILE_SIZE;
-//		
-//		g2.fillRect(x, y, TILE_SIZE, TILE_SIZE);
-//	}
-//	
-//	public void moveBy(int dRow, int dCol) {
-//		this.row += dRow;
-//		this.col += dCol;
-//	}
 
 }
--- CSSE220-Final-Project-Starter-main/src/model/Player.java
+++ CSSE220-Final-Project-Starter-main/src/model/Player.java
@@ -1,39 +1,9 @@
 package model;
 
-import java.awt.Graphics2D;
-
-public class Player extends Entity{
-	
-	private static final int TILE_SIZE = 40;
+public class Player extends Entity {
 	
 	public Player(int row, int col) {
 		super(row, col);
-	}
-
-//	/**
-//	 * @return the row
-//	 */
-//	public int getRow() {
-//		return row;
-//	}
-//
-//	/**
-//	 * @return the col
-//	 */
-//	public int getCol() {
-//		return col;
-//	}
-//	
-	public void drawOn(Graphics2D g2, BufferedImage sprite) {
-		int x = getCol() * TILE_SIZE;
-		int y = getRow() * TILE_SIZE;
-		
-		g2.drawImage(sprite, x, y, TILE_SIZE, TILE_SIZE, null);
-	}
-	
-	public void moveBy(int dRow, int dCol) {
-		setRow(getRow() + dRow);
-		setCol(getCol() + dCol);
 	}
 
 }
