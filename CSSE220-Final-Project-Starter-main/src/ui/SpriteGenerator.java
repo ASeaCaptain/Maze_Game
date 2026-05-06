24e5078a22835cf7f0d8af0b3e0aeab1cd70e8b6
rhit-jfang <jerryfang954@gmail.com>
2026-05-05 21:00:31 -0400

    Update SpriteGenerator.java
    Saved SpriteGenerator

--- CSSE220-Final-Project-Starter-main/src/ui/SpriteGenerator.java
+++ CSSE220-Final-Project-Starter-main/src/ui/SpriteGenerator.java
@@ -1,4 +1,34 @@
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
 
 public class SpriteGenerator {
 
+    public BufferedImage generatePlayerSprite() {
+        BufferedImage img = new BufferedImage(32, 32, BufferedImage.TYPE_INT_ARGB);
+        Graphics2D g2d = img.createGraphics();
+        
+        g2d.setColor(new Color(116, 73, 54));
+        g2d.fillRect(8, 4, 16, 16);
+        
+        g2d.setColor(new Color(0, 168, 168));
+        g2d.fillRect(8, 20, 16, 12);
+        
+        g2d.dispose();
+        return img;
+    }
+
+    public BufferedImage generateZombieSprite() {
+        BufferedImage img = new BufferedImage(32, 32, BufferedImage.TYPE_INT_ARGB);
+        Graphics2D g2d = img.createGraphics();
+        
+        g2d.setColor(new Color(65, 113, 40));
+        g2d.fillRect(8, 4, 16, 16);
+        
+        g2d.setColor(new Color(0, 168, 168));
+        g2d.fillRect(8, 20, 16, 12);
+        
+        g2d.dispose();
+        return img;
+    }
 }
