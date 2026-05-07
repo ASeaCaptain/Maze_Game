5dedc1e3286eb1679963e9f6d01df1a289635fc4
rhit-jfang <jerryfang954@gmail.com>
2026-05-07 14:20:31 -0400

    updated gamecomponent

--- CSSE220-Final-Project-Starter-main/src/ui/GameComponent.java
+++ CSSE220-Final-Project-Starter-main/src/ui/GameComponent.java
@@ -9,26 +9,33 @@ import model.GameModel;
 
 public class GameComponent extends JComponent {
 
-	
-	
 	private GameModel model;
+	private SpriteGenerator sprites;
+	private BufferedImage playerSprite;
+	private BufferedImage zombieSprite;
 
 
 	public GameComponent(GameModel model) {
-	this.model = model;
+		this.model = model;
+		this.sprites = new SpriteGenerator();
+		this.playerSprite = sprites.generatePlayerSprite();
+		this.zombieSprite = sprites.generateZombieSprite();
 	}
-
 
 	@Override
 	protected void paintComponent(Graphics g) {
-	super.paintComponent(g);
-	Graphics2D g2 = (Graphics2D) g;
+		super.paintComponent(g);
+		Graphics2D g2 = (Graphics2D) g;
 
 	// Minimal placeholder to test  it’s running
-	g2.drawString("Final Project Starter: UI is running ✅", 20, 30);
+		g2.drawString("Final Project Starter: UI is running ✅", 20, 30);
 
 
 	// TODO: draw based on model state
-	model.getPlayer().drawOn(g2);
+		model.getPlayer().drawOn(g2, playerSprite);
+		
+		for (Enemy enemy : model.getEnemies()) {
+			enemy.drawOn(g2, zombieSprite);
+		}
 	}
 }
